201e27ce4e5dced94048756e13a76d06f514469b
Tamás Bogdán <born2shred92@gmail.com>
2019-05-27 23:06:15 +0200

    Apperantly i can't spell

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@
 
 if (process.env.NODE_ENV === 'production') {
     // we are in production, return the prod set of keys
-    module.export = require('./prod');
+    module.exports = require('./prod');
 } else {
     // we are in development, return the dev set of keys
-    module.export = require('./dev');
+    module.exports = require('./dev');
 }
